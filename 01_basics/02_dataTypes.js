b8977594580dd3653a0e269c427404c90e69a178
TanushSaini <102867560+TanushSaini@users.noreply.github.com>
2025-04-14 12:21:39 +0000

    Summary of data types, value and reference type

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,7 @@
-/* {primitive datatypes}
+/* 
+primitive / value types = 7(string, number, boolean, null, undefined, symbol, BigInt)
+Non-primitive / reference types = (arrays, objects, functions)
+
 int => 2 to power 53
 bigint
 string => ""
@@ -7,10 +10,24 @@ null => standalone value(when value is empty)
 undefined => when value is not defined/assigned
 symbol => unique
 
+Non-primitive / reference types
 object
+arrays
+function
 */
 
 console.log(typeof null) //object
 console.log(typeof undefined) //undefined
 
 //NaN => Not a Number => comes when we try to convert a string that cannot be converted into a number, also when the output is not a number
+
+
+//array
+const heroes = ["RDJ", "Thor", "Hulk"]
+
+//object
+let myObj = {
+    name:"Tanush",
+    age: 21,
+    Mobile: 9981797559
+}
