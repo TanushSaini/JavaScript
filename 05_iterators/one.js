abc48f8f2351f1f3d76fbca5c11ffc7fbea5d68f
TanushSaini <102867560+TanushSaini@users.noreply.github.com>
2025-04-19 10:04:39 +0000

    loops

---
+++ 05_iterators/one.js
@@ -0,0 +1,17 @@
+//for
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(i);
+    
+    
+}
+
+//while
+while (condition) {
+    
+}
+
+//do while
+do {
+    
+} while (condition);
