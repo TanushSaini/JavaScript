ef5376b2e979465683f6184c7ea44598b66c0d37
TanushSaini <102867560+TanushSaini@users.noreply.github.com>
2025-04-21 13:20:59 +0000

    difference bw filter and for each

---
+++ 05_iterators/filter.js
@@ -0,0 +1,21 @@
+//for each loop doesnt return a value as shown below
+// const coding = ["js","python","cpp","java","ruby"]
+
+// const values = coding.forEach( (item) => {
+//     return item
+// } )  
+// console.log(values); //It gives undefined as foreach do not return any value
+
+//filter
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.filter( (item) => {
+//     return item > 4
+// } )
+//const newNums = myNums.filter( (item) => item>4 )
+
+//printing the same thing using for each loop
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if(num > 4) newNums.push(num)
+// } )
+//console.log(newNums);
